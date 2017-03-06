1eb619aea4b4cdef0407154de0b2606d4405a91d
王洪博 <25458269512qq.com3>
2017-03-06 16:14:04 +0800

    once

---
+++ src/main/java/text.java
@@ -0,0 +1,10 @@
+    /**
+     * Created by 王洪博 on 2017/3/6.
+     */
+public class text {
+        public static void main(String[] args) {
+            System.out.println("第一次写代码，感觉很爽！！！漂亮");
+        }
+
+
+}
